cec5dfd958ecc37045d6823d81ad9d2d342c90c2
Drew Russell <drewrussell@win2012-build-4.colo.rubrik.com>
2020-05-28 18:12:18 -0500

    add gql support

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -52,72 +48,72 @@ function TabPanel(props) {
     </div>
   );
 }
-export const FullScreenDialog = React.memo(
-  ({ key, responseBody, requestBody, closeRequestBody }) => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(true);
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
+export default function FullScreenDialog({
+  key,
+  responseBody,
+  requestBody,
+  closeRequestBody,
+}) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
 
-    const handleClose = () => {
-      closeRequestBody();
-      setOpen(false);
-    };
+  const handleClose = () => {
+    closeRequestBody();
+    setOpen(false);
+  };
 
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    return (
-      <div>
-        <Dialog
-          disablePortal
-          fullScreen
-          open={open}
-          onClose={handleClose}
-          TransitionComponent={Transition}
-        >
-          <AppBar className={classes.appBar} style={{ background: "#1DA1DC" }}>
-            <Toolbar variant="dense">
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={handleClose}
-                aria-label="close"
-              >
-                <CloseIcon />
-              </IconButton>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="simple tabs example"
-                indicatorColor="#000"
-              >
-                <Tab label="Response Body" />
-                <Tab label="Request Body" />
-              </Tabs>
+  return (
+    <div>
+      <Dialog
+        disablePortal
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar} style={{ background: "#1DA1DC" }}>
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="simple tabs example"
+              indicatorColor="#000"
+            >
+              <Tab label="Response Body" />
+              <Tab label="Request Body" />
+            </Tabs>
 
-              {/* <Button autoFocus color="inherit" onClick={handleClose}>
+            {/* <Button autoFocus color="inherit" onClick={handleClose}>
               Copy
             </Button> */}
-            </Toolbar>
-          </AppBar>
-          <TabPanel value={value} index={0}>
-            <SyntaxHighlighter language="json" style={githubGist}>
-              {JSON.stringify(responseBody, null, 2)}
-            </SyntaxHighlighter>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            <SyntaxHighlighter language="json" style={githubGist}>
-              {JSON.stringify(requestBody, null, 2)}
-            </SyntaxHighlighter>
-          </TabPanel>
-        </Dialog>
-      </div>
-    );
-  }
-);
+          </Toolbar>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <SyntaxHighlighter language="json" style={githubGist}>
+            {JSON.stringify(responseBody, null, 2)}
+          </SyntaxHighlighter>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <SyntaxHighlighter language="json" style={githubGist}>
+            {requestBody}
+          </SyntaxHighlighter>
+        </TabPanel>
+      </Dialog>
+    </div>
+  );
+}
